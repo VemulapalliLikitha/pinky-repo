b51e259b7956d31fc4c112c033f462d4815e49f6
saporedd <saporedd@DIN73003017.corp.capgemini.com>
2018-08-06 14:47:45 +0530

    second changes

--- Day1-GitDemo/src/main/java/org/cap/demo/SalesClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/SalesClass.java
@@ -5,9 +5,10 @@ int[] arr= {1,2,3,-89,56};
 
 public void show() {
 	int sum=0;
-	for(int num: arr) {
-		sum+=num;
-		System.out.println(num);
+	
+	for(int i=0;i<arr[i];i++) {
+		
+		System.out.println(arr[i]);
 		
 	}
 	System.out.println(sum);
