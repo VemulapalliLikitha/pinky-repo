6ddc8f18c57ddc23562eab5d8ddea2e0d943f5a2
saporedd <saporedd@DIN73003017.corp.capgemini.com>
2018-08-06 14:29:17 +0530

    Commit changes made

--- Day1-GitDemo/src/main/java/org/cap/demo/SalesClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/SalesClass.java
@@ -4,10 +4,13 @@ public class SalesClass {
 int[] arr= {1,2,3,-89,56};
 
 public void show() {
+	int sum=0;
 	for(int num: arr) {
+		sum+=num;
 		System.out.println(num);
+		
 	}
-	
+	System.out.println(sum);
 }
 
 
